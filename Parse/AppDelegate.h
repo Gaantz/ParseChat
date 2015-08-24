a0461cd2f55b26182c315635fdd1efa175f54196
cristian <cp@mambo.pe>
2015-08-24 15:50:51 -0500

    Initial Commit

---
+++ Parse/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Parse
+//
+//  Created by Cristian on 24/08/15.
+//  Copyright (c) 2015 Cristian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Parse/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Parse
+//
+//  Created by Cristian on 24/08/15.
+//  Copyright (c) 2015 Cristian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
